184722f61de32f97eb5eb0dfcbf486628aef1519
Louis-crypto <kinglouis745@gmail.com>
2020-01-29 03:00:53 +0300

    change method name

--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class PingPongTest {
 
     @Test
-    public void runPingPong_countUpToOne_ArrayList() {
+    public void runPinPong_countUpToOne_ArrayList() {
         PingPong testPingPong = new PingPong();
         List<Object> expectedOutput = new ArrayList<Object>();
         expectedOutput.add(1);
